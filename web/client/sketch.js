225bf3dfd1698d4b1931d76d530a5dc060327f09
tesselina <tesselina.spaeth@hs-augsburg.de>
2018-02-02 01:28:09 +0100

    real time view of knife position -sketch depicts ellipse on current coordinate

--- web/client/sketch.js
+++ web/client/sketch.js
@@ -5,7 +5,17 @@ https://www.intmath.com/blog/mathematics/length-of-an-archimedean-spiral-6595
 
 //var pxPerMm = 2;
 var spiralStruct = null;
-
+var dotIndex;
+var colors = [{
+  name: "blue", 
+  code: "#6b77ba"}, {
+  name: "red",
+  code: "#ce8575"}, {
+  name: "grey",
+  code: "#585353"}, {
+  name: "green",
+  code: "#a1bdb2"
+    }];
 
 function setup() {
   // put setup code here
@@ -13,17 +23,29 @@ function setup() {
   canvas.id("plate");
   canvas.parent('preview');
   canvas.class("blue");
-  noLoop();
 }
 
 socket.on('spiralStruct', function(struct){
   spiralStruct = struct;
-  loop();
+  });
+
+
+  socket.on('clientStream', function(index){
+    print('event', index);
+    dotIndex = index;
   });
 
 function draw() {
+  var dotColor; 
+  for (var i = 0; i < colors.length; i++){
+    if(canvas.class().includes(colors[i].name)){
+      (i == colors.length -1)? dotColor = colors[0].code : dotColor = colors[i+1].code;
+    }
+  }
+
   canvas.clear();
   translate(width/2, height/2);
+  scale(-1.0,1.0);  
   strokeWeight(1);
   stroke(255);
   noFill();
@@ -35,7 +57,13 @@ function draw() {
       vertex(x,y);
       }
     endShape();
-
+    if(dotIndex){
+      print('draw', dotIndex);
+      noStroke();
+      fill(dotColor);
+      var xD = spiralStruct[dotIndex].r * cos(spiralStruct[dotIndex].t);
+      var yD = spiralStruct[dotIndex].r * sin(spiralStruct[dotIndex].t);
+      ellipse(xD,yD,10);
+    }
   }
-  noLoop();
 }
